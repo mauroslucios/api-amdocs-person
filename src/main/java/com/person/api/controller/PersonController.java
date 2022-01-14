b55477a192f657f8dce4be645574f96fece02fbb
mauroslucios <mauroslucios@gmail.com>
2022-01-14 10:20:04 -0300

    Created GetPerson and PostPerson

--- src/main/java/com/person/api/controller/PersonController.java
+++ src/main/java/com/person/api/controller/PersonController.java
@@ -1,13 +1,23 @@
 package com.person.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.person.api.dto.PersonDTO;
+import com.person.api.entities.Person;
 import com.person.api.service.PersonService;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value="/api/v1")
@@ -20,7 +30,23 @@ public class PersonController {
 	
 	@Autowired
 	public PersonController(PersonService personService) {
-	
+		this.personService = personService;
 	}
+	
+	@GetMapping("/person")
+	@ApiOperation(value="Return a list of people")
+	public List<PersonDTO> listPerson(){
+		return personService.listPerson();
+	}
+	
+	@ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value="/person")
+    @ApiOperation(value="Create a person")
+	public Person insertPerson(@RequestBody Person person) {
+		return personService.insertPerson(person);
+	}
+	
+	
+	
 	
 }
--- src/main/java/com/person/api/service/PersonService.java
+++ src/main/java/com/person/api/service/PersonService.java
@@ -1,8 +1,12 @@
 package com.person.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.person.api.dto.PersonDTO;
+import com.person.api.entities.Person;
 import com.person.api.repository.PersonRepository;
 
 @Service
@@ -13,5 +17,14 @@ public class PersonService {
 	@Autowired
 	public PersonService(PersonRepository personRepository) {
 		this.personRepository = personRepository;
+	}
+
+	public List<PersonDTO> listPerson() {
+		List<Person> persons = personRepository.findAll();
+		return PersonDTO.converter(persons);
+	}
+
+	public Person insertPerson(Person person) {
+		return personRepository.save(person);
 	}
 }
